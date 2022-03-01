96027a8b2976d559066dfc78555549557358fe3c
perrynzhou <perrynzhou@gmail.com>
2022-03-01 08:46:26 +0800

    update and fixed path

---
+++ cmd/server/server.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"conf-server/drpc/kv"
+	"conf-server/drpc/pb"
+	"conf-server/util"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+)
+
+var (
+	sockFile = flag.String("s", "/tmp/drpc_socket.sock", "local socekt file")
+	port     = flag.Int("p", 50051, "The server port")
+)
+
+func init() {
+	logHook := util.NewHook()
+	logHook.Field = "line"
+	log.AddHook(logHook)
+}
+
+func main() {
+	flag.Parse()
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	kvService := kv.NewKvService(*sockFile)
+	pb.RegisterKvServiceServer(s, kvService)
+	log.Printf("server listening at %v", lis.Addr())
+	go func() {
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	for {
+		select {
+		case <-sigs:
+			log.Info("stop meta server....")
+			return
+		}
+	}
+}
